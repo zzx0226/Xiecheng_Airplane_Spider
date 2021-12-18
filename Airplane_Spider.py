07b9b2f6cb62804914965231b0817c291f690310
zzx0226 <zzx0226@hotmail.com>
2021-12-18 18:01:51 +0800

    2021.12.18

--- Airplane_Spider.py
+++ Airplane_Spider.py
@@ -1,29 +1,20 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 import time
-import re
 from prettytable import PrettyTable
-import string
-import urllib.request
 from email.header import Header
 from email.mime.text import MIMEText
 from email.utils import parseaddr, formataddr
 import smtplib
 import os
-
-
-
 #在此设置需要查取得日期
-ap_date=["2019-01-03","2019-01-04"]
+ap_date = ["2022-01-09", "2022-01-10"]
 #在此设置出发、到达城市 需要从携程查看相应城市代码
-depart_city="SHA"
-arrive_city="LHW"
+depart_city = "SHA"
+arrive_city = "BJS"
 #在此设置机票阈值
-expect_price=800
+expect_price = 1000
 
 #以下为邮箱设置
 from_addr = '*****@qq.com'
@@ -32,9 +23,11 @@ to_addr = '*******@hotmail.com'
 cc_addr = '*******@qq.com'
 smtp_server = 'smtp.qq.com'
 
+
 def _format_addr(s):
     name, addr = parseaddr(s)
     return formataddr((Header(name, 'utf-8').encode(), addr))
+
 
 def send_mail(data_html):
 
@@ -44,54 +37,57 @@ def send_mail(data_html):
     msg['Subject'] = Header('抢购机票啦！', 'utf-8').encode()
     msg['Cc'] = _format_addr('<%s>' % cc_addr)
 
-    server = smtplib.SMTP(smtp_server,587)
+    server = smtplib.SMTP(smtp_server, 587)
     server.set_debuglevel(1)
     server.starttls()
     server.login(from_addr, password)
-    server.sendmail(from_addr, [to_addr,cc_addr], msg.as_string())
+    server.sendmail(from_addr, [to_addr, cc_addr], msg.as_string())
     server.quit()
 
 
+options = webdriver.EdgeOptions()
+options.add_argument('--headless')  #
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-dev-shm-usage')
+driverfile_path = 'edgedriver_linux64/msedgedriver'  #这里填edgedriver的地址
+browser = webdriver.Edge(executable_path=driverfile_path, options=options)
 
-#options = webdriver.ChromeOptions()
-#options.add_argument('--headless')#
-#options.add_argument('--no-sandbox') 
-#options.add_argument('--disable-gpu')
-#options.add_argument('--disable-dev-shm-usage')
-#browser = webdriver.Chrome(chrome_options=options)
-
-
-browser=webdriver.Chrome()
-
+js_down = "var q=document.documentElement.scrollTop=10000"
+js_top = "var q=document.documentElement.scrollTop=0"
 airplane_table = PrettyTable(["航空公司", "出发地", "到达地", "出发时间", "到达时间", "价格"])
-for yd_date in range(len(ap_date)):
-    print(ap_date[yd_date])
-    airplane_table.add_row(['----------------','--------------',ap_date[yd_date],'-----------','-----------','-----------'])
-    browser.get("http://flights.ctrip.com/booking/"+depart_city+"-"+arrive_city+"-day-1.html?DDate1="+ap_date[yd_date])
-    element = browser.find_elements("css selector", ".search_table_header")
+for yd_date in ap_date:
+    print(f'{yd_date},expect price 800')
+    airplane_table.add_row(['----------------', '--------------', yd_date, '-----------', '-----------', '-----------'])
 
-    for i in range(len(element)):
-        plane_data_temp=element[i].text
-        plane_data=plane_data_temp.split()
+    browser.get(f"https://flights.ctrip.com/online/list/oneway-{depart_city}-{arrive_city}?_=1&depdate={yd_date}")
 
-        plane_al=plane_data[0]
-        plane_dt=plane_data[3]
-        plane_dpbn=plane_data[4]
-        plane_at=plane_data[5]
-        plane_apbn=plane_data[6]
-        plane_lp=re.sub("\D", "", str(plane_data[9]))
-        try:
-            plane_lp_int=int(plane_lp)
-        except ValueError:
-            pass
-        airline_al=plane_al[0:4]
-        if plane_lp_int<expect_price:
-            airplane_table.add_row([plane_al,plane_dpbn,plane_apbn,plane_dt,plane_at,plane_lp])
-        print(plane_data)
-    time.sleep(10)#此处不建议注释 以防被封
+    browser.execute_script(js_down)  #滚动网页 使得数据加载完毕
+    browser.implicitly_wait(5)
+    browser.execute_script(js_down)
+    browser.implicitly_wait(2)
+    browser.execute_script(js_top)
+    browser.implicitly_wait(3)
+    browser.execute_script(js_down)
 
+    flight_detials = browser.find_elements(By.CLASS_NAME, 'flight-detail')
+    flight_prices = browser.find_elements(By.CLASS_NAME, 'flight-operate')
+    flight_airlines = browser.find_elements(By.CLASS_NAME, 'flight-airline')
+
+    for i, (flight_detial, flight_price, flight_airline) in enumerate(zip(flight_detials, flight_prices, flight_airlines)):
+        flight_detial_text = flight_detial.get_attribute('innerText')
+        flight_airline = flight_airline.get_attribute('innerText').split('\n')[0]
+        try:  #经停航班会有更多的返回值 不考虑经停航班
+            plane_dt, plane_dl, plane_at, plane_al = flight_detial_text.split("\n")
+        except:
+            continue
+        Price = flight_price.get_attribute('innerText').split('\n')[0][1:-1]
+        if int(Price) < expect_price:
+            airplane_table.add_row([flight_airline, plane_dl, plane_al, plane_dt, plane_at, Price])
+    time.sleep(10)
+print(airplane_table)
 
 data_html = airplane_table.get_html_string()
-send_mail(data_html)
+# send_mail(data_html)
 browser.close()
-os.system('taskkill /im chromedriver.exe /F')
+# os.system('taskkill /im edgedriver.exe /F')
--- README.md
+++ README.md
@@ -1,13 +1,24 @@
 # Xiecheng_Airplane_Spide
-
+# Update 2021/12/08
 简介
 ----
-该爬虫使用 PythonSelenium+Chorme爬取携程机票，如果有符合标准的机票，通过邮件发送通知。
+该爬虫使用 Python Selenium+Edge爬取携程机票，如果有符合标准的机票，通过邮件发送通知。
+![](Result.png)
 
 环境
 ----
 
-Python3.6 Chrom70.0.3538.77及相应版本的chromedirver
+Python3.9 Edge 97.0.1072.34及相应版本的edgedirver
+
+
+简介
+----
+该爬虫使用 Python Selenium+Chorme爬取携程机票，如果有符合标准的机票，通过邮件发送通知。
+
+环境
+----
+
+Python3.6 Chrome70.0.3538.77及相应版本的chromedirver
 
 好用的话就点个star吧
 
